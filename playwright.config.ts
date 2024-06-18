4ab6ecd4365792e02beb05a4f8492d920db152fd
vananhhoang <anhhtv.93@gmail.com>
2024-06-18 10:57:37 +0200

    update with new test case

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,14 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    process.env.CI ? ["dot"] : ["list"],
+    [
+      "@argos-ci/playwright/reporter",
+      {
+        // Upload to Argos on CI only.
+        uploadToArgos: !!process.env.CI,
+      },
+    ],
     ['html']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -32,6 +40,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // Capture screenshot after each test failure.
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
--- tests/testWithCI.spec.ts
+++ tests/testWithCI.spec.ts
@@ -1,5 +1,6 @@
 import test from "@playwright/test"
-import { NavigationPage } from "./page-object/navigationPage"
+import { argosScreenshot } from "@argos-ci/playwright";
+/*import { NavigationPage } from "./page-object/navigationPage"
 
 test.beforeEach(async({page}) => {
     test.setTimeout(10000);
@@ -10,4 +11,22 @@ test('testing with argos ci', async({page})=>{
     const navigateTo = new NavigationPage(page)
     await navigateTo.moveToFormLayoutsPage()
     await navigateTo.moveToDatepickerPage()
+})*/
+
+test('drag and drop with ifame argo ci', async({page})=>{
+    await page.goto(process.env.URL)
+
+    await argosScreenshot(page, "homepage");
+
+    const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
+    await frame.locator('li', {hasText: "High Tatras 2"}).dragTo(frame.locator('#trash'))
+
+    await argosScreenshot(page, "1stDrag");
+
+    // more precise control
+    await frame.locator('li', {hasText: "High Tatras 4"}).hover()
+    await page.mouse.down()
+    await frame.locator('#trash').hover()
+    await page.mouse.up()
+    await argosScreenshot(page, "2ndDrag");
 })
