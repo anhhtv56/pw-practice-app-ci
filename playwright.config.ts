92c67ff9dc5264624bdcc5b94096eaca0d560f8a
vananhhoang <anhhtv.93@gmail.com>
2024-06-20 12:51:50 +0200

    add webServer to start server and update test case for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -90,6 +90,12 @@ export default defineConfig({
     // },
   ],
 
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200/',
+    timeout: 120 * 1000,
+  }
+
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
